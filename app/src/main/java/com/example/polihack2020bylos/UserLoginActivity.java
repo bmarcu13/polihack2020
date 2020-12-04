cc2b641a5df72281a7d874e0722478e787b1425d
Bogdan <bogdanmarcu2003@gmail.com>
2020-12-04 17:45:40 +0200

    Completed user signup and login functions.

---
+++ app/src/main/java/com/example/polihack2020bylos/UserLoginActivity.java
@@ -0,0 +1,89 @@
+package com.example.polihack2020bylos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserLoginActivity extends AppCompatActivity {
+
+    private EditText emailField, passField;
+    private Button loginButton;
+    private FirebaseAuth fAuth;
+    private FrameLayout loadingScreen;
+    private TextView create_an_account_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_login);
+
+        fAuth = FirebaseAuth.getInstance();
+        emailField = findViewById(R.id.email_field);
+        passField = findViewById(R.id.password_field);
+        loginButton = findViewById(R.id.login_button);
+        loadingScreen = findViewById(R.id.loadingOverlay);
+        create_an_account_button = findViewById(R.id.create_account_button);
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password;
+
+                email = emailField.getText().toString().trim();
+                password = passField.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    emailField.setError("This field can't be empty.");
+                    return;
+                }
+                if (password.isEmpty()) {
+                    passField.setError("This field can't be empty.");
+                    return;
+                }
+
+                loadingScreen.setVisibility(ConstraintLayout.VISIBLE);
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(UserLoginActivity.this, "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                            loadingScreen.setVisibility(ConstraintLayout.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+
+        create_an_account_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserLoginActivity.this, UserSignupActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/polihack2020bylos/UserSignupActivity.java
@@ -0,0 +1,101 @@
+package com.example.polihack2020bylos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserSignupActivity extends AppCompatActivity {
+
+    private EditText emailField, passField, confirmPassField;
+    private Button signupButton;
+    private FirebaseAuth fAuth;
+    private FrameLayout loadingScreen;
+    private TextView goToLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_register);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        emailField = findViewById(R.id.email_field);
+        passField = findViewById(R.id.password_field);
+        confirmPassField = findViewById(R.id.confirm_password_field);
+
+        signupButton = findViewById(R.id.signup_button);
+        loadingScreen = findViewById(R.id.loadingOverlay);
+
+        goToLogin = findViewById(R.id.go_to_login);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email, password, passwordConfirmation;
+
+                email = emailField.getText().toString().trim();
+                password = passField.getText().toString().trim();
+                passwordConfirmation = confirmPassField.getText().toString().trim();
+
+                if (email.isEmpty()) {
+                    emailField.setError("This field can't be empty");
+                    return;
+                }
+
+                if (password.isEmpty()){
+                    passField.setError("This field can't be empty");
+                    return;
+                }
+                if (passwordConfirmation.isEmpty()) {
+                    confirmPassField.setError("This field can't be empty");
+                    return;
+                }
+                if (!password.equals(passwordConfirmation)) {
+                    passField.setError("The passwords don't match");
+                    return;
+                }
+                if (password.length() < 6) {
+                    passField.setError("Password must be at least 6 characters long");
+                    return;
+                }
+                loadingScreen.setVisibility(ConstraintLayout.VISIBLE);
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            startActivity(new Intent(UserSignupActivity.this, MainActivity.class));
+                            finish();
+                        }
+                        else {
+                            Toast.makeText(UserSignupActivity.this, "Error:" + task.getException(), Toast.LENGTH_SHORT).show();
+                            loadingScreen.setVisibility(ConstraintLayout.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
+
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
