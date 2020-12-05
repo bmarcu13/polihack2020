d8e8e4f289e6e6d700da37036c0ba75f2f98f79d
Bogdan <bogdanmarcu2003@gmail.com>
2020-12-05 21:04:59 +0200

    Completed patient display activity

--- app/src/main/java/com/example/polihack2020bylos/DoctorApp/AddPatient/Patient.java
+++ app/src/main/java/com/example/polihack2020bylos/DoctorApp/AddPatient/Patient.java
@@ -8,25 +8,25 @@ public class Patient{
     private String id;
     private String name;
     private Integer age;
-//    private String sex;
-//    private String blood_type;
-//    private ArrayList<String> symptomes;
-//    private ArrayList<String> treatment;
-//    private ArrayList<String> side_effects;
-//    private boolean feeling_better;
+    private String sex;
+    private String blood_type;
+    private ArrayList<String> symptomps;
+    private ArrayList<String> medication;
+    private ArrayList<String> side_effects;
+    private boolean got_better;
 
     private Patient(){}
 
-    public Patient(String id, String name, Integer age, String sex, String blood_type, ArrayList<String> symptomes, ArrayList<String> treatment, ArrayList<String> side_effects, boolean feeling_better) {
+    public Patient(String id, String name, Integer age, String sex, String blood_type, ArrayList<String> symptomps, ArrayList<String> medication, ArrayList<String> side_effects, boolean got_better) {
         this.id = id;
         this.name = name;
         this.age = age;
-//        this.sex = sex;
-//        this.blood_type = blood_type;
-//        this.symptomes = symptomes;
-//        this.treatment = treatment;
-//        this.side_effects = side_effects;
-//        this.feeling_better = feeling_better;
+        this.sex = sex;
+        this.blood_type = blood_type;
+        this.symptomps = symptomps;
+        this.medication = medication;
+        this.side_effects = side_effects;
+        this.got_better = got_better;
     }
 
     public String getId() {
@@ -53,51 +53,51 @@ public class Patient{
         this.age = age;
     }
 
-//    public String getSex() {
-//        return sex;
-//    }
-//
-//    public void setSex(String sex) {
-//        this.sex = sex;
-//    }
-//
-//    public String getBlood_type() {
-//        return blood_type;
-//    }
-//
-//    public void setBlood_type(String blood_type) {
-//        this.blood_type = blood_type;
-//    }
-//
-//    public ArrayList<String> getSymptomes() {
-//        return symptomes;
-//    }
-//
-//    public void setSymptomes(ArrayList<String> symptomes) {
-//        this.symptomes = symptomes;
-//    }
-//
-//    public ArrayList<String> getTreatment() {
-//        return treatment;
-//    }
-//
-//    public void setTreatment(ArrayList<String> treatment) {
-//        this.treatment = treatment;
-//    }
-//
-//    public ArrayList<String> getSide_effects() {
-//        return side_effects;
-//    }
-//
-//    public void setSide_effects(ArrayList<String> side_effects) {
-//        this.side_effects = side_effects;
-//    }
-//
-//    public boolean isFeeling_better() {
-//        return feeling_better;
-//    }
-//
-//    public void setFeeling_better(boolean feeling_better) {
-//        this.feeling_better = feeling_better;
-//    }
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getBlood_type() {
+        return blood_type;
+    }
+
+    public void setBlood_type(String blood_type) {
+        this.blood_type = blood_type;
+    }
+
+    public ArrayList<String> getSymptomps() {
+        return symptomps;
+    }
+
+    public void setSymptomps(ArrayList<String> symptomps) {
+        this.symptomps = symptomps;
+    }
+
+    public ArrayList<String> getmedication() {
+        return medication;
+    }
+
+    public void setmedication(ArrayList<String> medication) {
+        this.medication = medication;
+    }
+
+    public ArrayList<String> getSide_effects() {
+        return side_effects;
+    }
+
+    public void setSide_effects(ArrayList<String> side_effects) {
+        this.side_effects = side_effects;
+    }
+
+    public boolean isgot_better() {
+        return got_better;
+    }
+
+    public void setgot_better(boolean got_better) {
+        this.got_better = got_better;
+    }
 }
--- app/src/main/java/com/example/polihack2020bylos/DoctorApp/PatientList/PatientDisplayActivity.java
+++ app/src/main/java/com/example/polihack2020bylos/DoctorApp/PatientList/PatientDisplayActivity.java
@@ -2,15 +2,88 @@ package com.example.polihack2020bylos.DoctorApp.PatientList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.polihack2020bylos.R;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class PatientDisplayActivity extends AppCompatActivity {
+
+    private TextView nameTextView, ageTextView, sexTextView, bloodTypeTextView;
+    private TextView symptomsTextView, sideEffectsTextView, medicationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_display);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        Integer age = intent.getIntExtra("age", 404);
+        String sex = intent.getStringExtra("sex");
+        String blood_type = intent.getStringExtra("blood_type");
+        Boolean gotBetetr = intent.getBooleanExtra("blood_type", true);
+        ArrayList<String> symptoms = intent.getStringArrayListExtra("symptomps");
+        ArrayList<String> medication = intent.getStringArrayListExtra("medication");
+        ArrayList<String> sideEffects = intent.getStringArrayListExtra("side_effects");
+
+        nameTextView = findViewById(R.id.patient_name);
+        ageTextView = findViewById(R.id.patient_age);
+        bloodTypeTextView = findViewById(R.id.patient_blood_type);
+        sexTextView = findViewById(R.id.patient_sex);
+        symptomsTextView = findViewById(R.id.symptoms_textview);
+        sideEffectsTextView = findViewById(R.id.side_effects_textview);
+        medicationTextView = findViewById(R.id.medication_textview);
+
+        nameTextView.setText(name);
+        ageTextView.setText(Long.toString(age));
+        sexTextView.setText(sex);
+        bloodTypeTextView.setText(blood_type);
+
+        String symptomsList = "";
+        for (int i = 0; i < symptoms.size(); i++) {
+            symptomsList += symptoms.get(i);
+            if (i < symptoms.size() - 1) {
+                symptomsList += " • ";
+            }
+        }
+        symptomsTextView.setText(symptomsList);
+
+        String sideEffectsList = "";
+        for (int i = 0; i < symptoms.size(); i++) {
+            sideEffectsList += sideEffects.get(i);
+            if (i < symptoms.size() - 1) {
+                sideEffectsList += " • ";
+            }
+        }
+        sideEffectsTextView.setText(sideEffectsList);
+
+        String medicationList = "";
+        for (int i = 0; i < symptoms.size(); i++) {
+            medicationList += medication.get(i);
+            if (i < symptoms.size() - 1) {
+                medicationList += " • ";
+            }
+        }
+        medicationTextView.setText(medicationList);
+
+        Log.d("TAG", "onCreate: " + symptoms);
+        Log.d("TAG", "onCreate: " + medication);
+        Log.d("TAG", "onCreate: " + sideEffects);
+
+        findViewById(R.id.back_arrow).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
