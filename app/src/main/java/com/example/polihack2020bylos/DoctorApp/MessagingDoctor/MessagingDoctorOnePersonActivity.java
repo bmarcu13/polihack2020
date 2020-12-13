7ef02007f2c8fbb7321023dc6b6f1f49c12a00eb
iondragos <iondragos0909@yahoo.com>
2020-12-13 13:05:56 +0200

    mesaje tati

--- app/src/main/java/com/example/polihack2020bylos/DoctorApp/MessagingDoctor/MessagingDoctorOnePersonActivity.java
+++ app/src/main/java/com/example/polihack2020bylos/DoctorApp/MessagingDoctor/MessagingDoctorOnePersonActivity.java
@@ -3,14 +3,33 @@ package com.example.polihack2020bylos.DoctorApp.MessagingDoctor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.polihack2020bylos.R;
 
 public class MessagingDoctorOnePersonActivity extends AppCompatActivity {
 
+    ImageView back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging_doctor_one_person);
+
+        back = findViewById(R.id.back_arrow_messaging_one_p);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+        finish();
+    }
+
+
 }
